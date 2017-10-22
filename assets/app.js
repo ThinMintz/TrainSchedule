514b9ba40c633307ae2723e4fd107214814fb109
ThinMintz <rachel.rodriguez05@gmail.com>
2017-10-21 22:31:43 -0500

    train details appear on HTML, but they are all in one column. Need to work on Mins away

--- assets/app.js
+++ assets/app.js
@@ -37,4 +37,38 @@
     console.log(newTrain.firstTrain);
     console.log(newTrain.frequency);
   
+  //clears all of the text-boxes
+  $("#train-name").val("");
+  $("#destination-name").val("");
+  $("#first-train").val("");
+  $("#frequency-name").val("");
   });
+
+//creates firebase event for adding trains to schedule in the rows when the user adds a train
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+  console.log(childSnapshot.val());
+
+  var trainName = childSnapshot.val().train;
+  var destinationName = childSnapshot.val().destination;
+  var firstTrain = childSnapshot.val().firstTrain;
+  var frequency = childSnapshot.val().frequency;
+
+//train info
+  console.log(trainName);
+  console.log(destinationName);
+  console.log(firstTrain);
+  console.log(frequency);
+
+  // Prettify the train mins away
+  var trainMinsAway = moment.unix(firstTrain).format("MM:mm");
+
+  // Calculate the months worked using hardcore math
+  // To calculate the months worked
+  var trainTime = moment().diff(moment.unix(firstTrain, "X"), "minutes");
+  console.log(trainTime);
+
+// Add each train's data into the table
+  $("#train-table > tbody").append("<tr><td>" + trainName + "</tr></td>" + destinationName + "</tr><td>" + trainMinsAway + "</tr><td>" + frequency + "</tr></td>");
+
+});
